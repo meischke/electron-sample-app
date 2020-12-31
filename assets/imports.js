11887269b81cec08de71e7af7bdafe23d66f0435
Marko Meischke <marko.meischke@gmail.com>
2020-12-31 14:42:56 +0100

    add: directory import for template files

--- assets/imports.js
+++ assets/imports.js
@@ -1,28 +1,56 @@
-const links = document.querySelectorAll('link[rel="templateimport"]')
+const links = document.querySelectorAll('link[rel="templateimport"]');
 
-function getTemplate(filepath){
-  return new Promise(function(resolve, reject) {
-    fetch(filepath).then(function (response) {
-      // The API call was successful!
-      return response.text()
-    }).then(function (html) {
-      // If successful, resolve the promis and passing back the response
-      resolve(html);
-    }).catch(function (err) {
-      // There was an error
-      reject(err)
-    })
-  })
+function getTemplate(filepath) {
+  return new Promise(function (resolve, reject) {
+    fetch(filepath)
+      .then(function (response) {
+        // The API call was successful!
+        return response.text();
+      })
+      .then(function (html) {
+        // If successful, resolve the promis and passing back the response
+        resolve(html);
+      })
+      .catch(function (err) {
+        // There was an error
+        reject(err);
+      });
+  });
 }
 
-// Import and add each page to the DOM
+function appendHTMLtemplate(html) {
+  let doc = new DOMParser().parseFromString(html, "text/html");
+  let clone = document.importNode(
+    doc.querySelector(".section-template").content,
+    true
+  );
+  document.querySelector(".content").appendChild(clone);
+}
+
+// Import and add each HTML template to the DOM
 Array.prototype.forEach.call(links, (link) => {
-  getTemplate(link.href)
-    .then( function (html) {
-      let doc = new DOMParser().parseFromString(html, "text/html")
-      let clone = document.importNode(doc.querySelector('.section-template').content, true)
-      document.querySelector('.content').appendChild(clone)
-    }, function(err) {
-      console.log(err)
-    })
-})
+  if (typeof link.dataset.type !== "undefined" && link.dataset.type == "dir") {
+    let dir = link.href.replace("file://", "");
+    if (dir.slice(-1) == "/") dir = dir.slice(0, -1);
+    const files = myAPI.globSync("".concat(dir, "/**/*.html"));
+    files.forEach((file) => {
+      getTemplate(file).then(
+        function (html) {
+          appendHTMLtemplate(html);
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
+    });
+  } else {
+    getTemplate(link.href).then(
+      function (html) {
+        appendHTMLtemplate(html);
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  }
+});
--- renderer-process/index.preload.js
+++ renderer-process/index.preload.js
@@ -1,6 +1,8 @@
 const { ipcRenderer, contextBridge } = require("electron");
+const glob = require('glob')
 
 contextBridge.exposeInMainWorld("myAPI", {
+  globSync: (...args) => glob.sync(...args),
   ipcSend: (...args) => ipcRenderer.send(...args),
   ipcOn: (key, handler) =>
     ipcRenderer.on(key, (event, ...args) => handler(event, ...args)),
